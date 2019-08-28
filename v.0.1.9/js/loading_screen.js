42adafc2728d53045e18a657dbbc414af880487e
YungKellz <51682231+YungKellz@users.noreply.github.com>
2019-08-28 15:08:05 +0300

    Add files via upload

---
+++ v.0.1.9/js/loading_screen.js
@@ -0,0 +1,18 @@
+document.body.onload = function (){
+	console.log("beach");
+	setTimeout(function() {
+		var preloader = document.getElementById("preloader");
+		if (!preloader.classList.contains('loading_done2'))
+		{
+			preloader.classList.add('loading_done2');
+		}
+	}, 1700); 
+	setTimeout(function() {
+		var loader = document.getElementById("loader");
+		if (!loader.classList.contains('loading_done1'))
+		{
+			loader.classList.add('loading_done1');
+		}
+	}, 1000); 
+
+}
