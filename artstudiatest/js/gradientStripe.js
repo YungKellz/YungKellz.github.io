568a487ca78507dda4d210e6a0b3cc65b0ff6981
YungKellz <51682231+YungKellz@users.noreply.github.com>
2019-10-28 22:13:55 +0300

    test2

--- artstudiatest/js/gradientStripe.js
+++ artstudiatest/js/gradientStripe.js
@@ -12,15 +12,31 @@ window.setTimeout(function(){
 function getRandomInRange(min, max) {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+function getRandomInRangePRO(min, max, n) {
+	var a = getRandomInRange(min, max);
+	if (a>70 && a <110){
+		getRandomInRangePRO(min, max, n);
+	}
+	else{
+		if (a>70 && a <110){
+			getRandomInRangePRO(min, max, n);
+		}
+		else{
+			console.log(a);
+			return a;
+		}
+	}
+}
+
 function changeGradient1(){
-	gr1.style.background='linear-gradient('+ getRandomInRange(0, 270)+'deg, #E100FF '+ getRandomInRange(0, 20)+'%, #7F00FF 100%)';
+	gr1.style.background='linear-gradient('+ getRandomInRangePRO(0, 360, 1)+'deg, #E100FF '+ getRandomInRangePRO(0, 20, 1)+'%, #7F00FF 100%)';
 	gr1.style.opacity="1";
 	gr2.style.opacity="0";
-	console.log('sad');
 }
 function changeGradient2(){
-	gr2.style.background='linear-gradient('+ getRandomInRange(0, 270)+'deg, #E100FF '+ getRandomInRange(0, 20)+'%, #7F00FF 100%)';
+	gr2.style.background='linear-gradient('+ getRandomInRangePRO(0, 360, 2)+'deg, #E100FF '+ getRandomInRangePRO(0, 20, 2)+'%, #7F00FF 100%)';
 	gr1.style.opacity="0";
 	gr2.style.opacity="1";
-	console.log('asd');
+	console.log("changed");
 }
