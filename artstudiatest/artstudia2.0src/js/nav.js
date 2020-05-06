e280d8742b787d0bdcda1e774581206de357a7e4
YungKellz <51682231+YungKellz@users.noreply.github.com>
2020-05-06 13:57:58 +0300

    первые портфолио и наша жизнь

--- artstudiatest/artstudia2.0src/js/nav.js
+++ artstudiatest/artstudia2.0src/js/nav.js
@@ -17,7 +17,7 @@ function rndInt(min, max) {
 	nav_white.style.width = document.getElementsByClassName('container')[0].offsetWidth + "px";
 	foot_white.style.width = document.getElementsByClassName('container')[0].offsetWidth + "px";
 	foot_gradient.style.height = footer.offsetHeight - 80 + "px";
-	foot_white.style.height = footer.offsetHeight - 80 + "px";
+	foot_white.style.height = footer.offsetHeight + "px";
 	footer.addEventListener('change', () => {
 		foot_gradient.style.height = footer.offsetHeight - 80 + "px";
 		foot_white.style.height = footer.offsetHeight - 80 + "px";
@@ -26,46 +26,15 @@ function rndInt(min, max) {
 var whiteDrop = document.getElementById('nav_dropdown_white_bg');
 
 nav_menu_works.addEventListener("mouseover",function(){
-	// whiteDrop.style.visibility = "visible";
-	// whiteDrop.style.opacity = "1";
-	// whiteDrop.style.height = "400px";
-	//nav_white.style.height = "280px";
-	//nav_white.style.borderRadius = "0px 0px 50px 50px";
-	//nav_white.style.boxShadow = "0 10px 10px rgba(255,255,255,1)";
 	document.getElementsByTagName("main")[0].style.filter = "blur(3px)";
 	document.getElementsByTagName("main")[0].style.opacity = "0.3";
-	
-	// nav_menu_works_head.style.visibility = "hidden";
-	// nav_menu_works_head.style.opacity = "0";
-	// ulOfWorks.style.visibility = "visible";
-	// ulOfWorks.style.opacity = "1";
-	// #nav_menu_works_head{
-	// 	visibility: hidden;
-	// 	opacity: 0;
-	//   }
-	//   ul{
-	// 	visibility: visible;
-	// 	opacity: 1;
-	//   }
-	//nav_gradient.style.height = "270px";
-	//navigationOn.style.height = "270px"
 });
 nav_menu_works.addEventListener("mouseout",function(){
-	// whiteDrop.style.visibility = "hidden";
-	// whiteDrop.style.opacity = "0";
-	// whiteDrop.style.height = "300px";
-	//nav_white.style.transition = "0.2s 0.4s";
 	document.getElementsByTagName("main")[0].style.filter = "blur(0px)";
 	document.getElementsByTagName("main")[0].style.opacity = "1";
 	nav_white.style.height = "128px";
 	nav_white.style.borderRadius = "0px";
 	nav_white.style.boxShadow = "0 10px 10px rgba(255,255,255,0)";
-	// nav_menu_works_head.style.visibility = "visible";
-	// nav_menu_works_head.style.opacity = "1";
-	// ulOfWorks.style.visibility = "hidden";
-	// ulOfWorks.style.opacity = "0";
-	//nav_gradient.style.height = "128px";
-	//navigationOn.style.height = "128px"
 });
 
 /*Градиент*/
